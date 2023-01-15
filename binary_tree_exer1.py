e722a96acfa0972156e1e204caad942222ceba8a
Hylyyy <palisochydeelyn97@gmail.com>
2023-01-15 10:45:41 +0800

    if val, (right)

--- binary_tree_exer1.py
+++ binary_tree_exer1.py
@@ -32,3 +32,9 @@ class BinarySearchTreeNode:
                 return self.left.search(val)
             else:
                 return False
+        
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
