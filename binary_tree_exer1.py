9b53bdb523bf9b14b321018dbca38137db485470
Hylyyy <palisochydeelyn97@gmail.com>
2023-01-15 10:38:49 +0800

    def add child

--- binary_tree_exer1.py
+++ binary_tree_exer1.py
@@ -6,3 +6,7 @@ class BinarySearchTreeNode:
         self.data = data
         self.left = None
         self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            return 
