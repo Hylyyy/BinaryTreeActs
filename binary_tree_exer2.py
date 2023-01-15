1aff61db304c54bc8bdd0d53af3ded02481e643c
Hylyyy <palisochydeelyn97@gmail.com>
2023-01-15 12:10:14 +0800

    if data (left)

--- binary_tree_exer2.py
+++ binary_tree_exer2.py
@@ -10,3 +10,9 @@ class BinarySearchTreeNode:
     def add_child(self, data):
         if data == self.data:
             return 
+
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
