a5a356208b05e3c8f26c39d73ff004c4a89a6310
Hylyyy <palisochydeelyn97@gmail.com>
2023-01-15 12:52:45 +0800

    else

--- binary_tree_exer2.py
+++ binary_tree_exer2.py
@@ -57,3 +57,10 @@ class BinarySearchTreeNode:
         elif val > self.data:
             if self.right:
                 self.right = self.right.delete(val)
+        else:
+            if self.left is None and self.right is None:
+                return None
+            elif self.left is None:
+                return self.right
+            elif self.right is None:
+                return self.right
